7f81fee5f03644e2e78be385fa4194ad555a33f7
ala-kad <kaddechiala@gmail.com>
2025-05-14 19:03:39 +0100

    feat: config & install TypeORM +  MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,35 @@
+// Modules
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+// Config
+import { DataSource } from 'typeorm';
+import { Product } from './products/entities/product.entity';
 
 @Module({
-  imports: [AuthModule, ProductsModule],
+  imports: [
+    AuthModule, 
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nestcafe',
+      entities: [],
+      synchronize: true, // Set to false in production
+      autoLoadEntities: true,
+    }),
+    TypeOrmModule.forFeature([Product])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
