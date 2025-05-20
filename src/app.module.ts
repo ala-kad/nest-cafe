410feb112f80acba3963c2de8e9eb6aed20ddd4f
kamelnaifer17 <kamel.nfr@gmail.com>
2025-05-20 11:24:12 +0100

    fix: install and config of neondb and pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+        TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('NEON_DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true, 
+        ssl: {
+          rejectUnauthorized: false, 
+        },
+      }),
+            inject: [ConfigService],
+    }),
+
   controllers: [AppController],
   providers: [AppService],
 })
