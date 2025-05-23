a4fa2fde2b2d94007d9aba029bc97f4cec900036
ala-kad <kaddechiala@gmail.com>
2025-05-23 23:20:38 +0100

    feat: added Status enum for order status

---
+++ src/orders/enums/status.enum.ts
@@ -0,0 +1,12 @@
+export enum Status {
+  PENDING = 'pending',
+  IN_PROGRESS = 'in_progress',
+  COMPLETED = 'completed',
+  CANCELED = 'canceled',
+  REFUNDED = 'refunded',
+  FAILED = 'failed',
+  DELIVERED = 'delivered',
+  RETURNED = 'returned',
+  EXCHANGED = 'exchanged',
+  ON_HOLD = 'on_hold'
+}
