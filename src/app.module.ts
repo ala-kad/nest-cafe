0d87ceef4c8bc794349c550fe60305a675c3d4af
kamelnaifer17 <kamel.nfr@gmail.com>
2025-05-24 17:43:37 +0100

    fix:  added module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
-// Modules
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// Config
 import { DataSource } from 'typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -29,6 +27,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
     }),
     ProductsModule,
+    UsersModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
