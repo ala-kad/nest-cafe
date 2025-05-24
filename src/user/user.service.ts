7632455ecbe1466a602a9ce517c93dd729b45cc1
kamelnaifer17 <kamel.nfr@gmail.com>
2025-05-24 17:48:02 +0100

    refactor: simplify user service by removing unused methods and improving password handling

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,50 +1,29 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './entities/user.entity';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import * as bcrypt from 'bcrypt';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UserService {
-  constructor(
-    @InjectRepository(User)
-    private repository: Repository<User>,
-  ) {}
+export class UsersService {
+  constructor(@InjectRepository(User) private usersRepo: Repository<User>) {}
 
-async create(createUserDto: CreateUserDto): Promise<User> {
-  const { username, password, email } = createUserDto;
-
-  const salt = await bcrypt.genSalt(); 
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const user = this.repository.create({
-    username,
-    email,
-    password: hashedPassword,
-  });
-
-  return this.repository.save(user);
-}
-  findAll() {
-    return this.repository.find();
+  async findByUsername(username: string): Promise<User | null> {
+    return this.usersRepo.findOne({ where: { username } });
   }
 
-  async findOne(id: number) {
-    const user = await this.repository.findOne({ where: { id } });
-    if (!user) throw new NotFoundException('User not found');
-    return user;
+  async create(username: string, password: string): Promise<User> {
+    const hash = await bcrypt.hash(password, 10);
+    const user = this.usersRepo.create({ username, password: hash });
+    return this.usersRepo.save(user);
   }
 
-  async update(id: number, data: UpdateUserDto) {
-    const user = await this.findOne(id);
-    Object.assign(user, data);
-    return this.repository.save(user);
-  }
-
-  async remove(id: number) {
-    const user = await this.findOne(id);
-    return this.repository.remove(user);
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.findByUsername(username);
+    if (user && await bcrypt.compare(password, user.password)) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
   }
 }
