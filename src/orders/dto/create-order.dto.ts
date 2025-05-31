7581de4aee2cb26e03f264ee6d6a59a4dde1b53c
ala-kad <kaddechiala@gmail.com>
2025-05-31 18:18:04 +0100

    fix: Order DTO removed class-validator

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,7 +1,2 @@
-import { IsOptional } from 'class-validator';
-import { Product } from 'src/products/entities/product.entity';
-
 export class CreateOrderDto {
-  @IsOptional()
-  orderItems: Product[];
 }
